b562b4a584226d639e3a8b45286b824cbc77df0a
melod96 <40789417+melod96@users.noreply.github.com>
2018-09-18 15:48:33 +0900

    Update MemberDao.java

--- src/com/comvision/artBridge/member/model/dao/MemberDao.java
+++ src/com/comvision/artBridge/member/model/dao/MemberDao.java
@@ -37,8 +37,7 @@ private Properties prop = new Properties();
 		try {
 			pstmt = con.prepareStatement(query);
 			pstmt.setString(1, userId);
-			pstmt.setString(2, userId);
-			
+			pstmt.setString(2, userId);	
 			pstmt.setString(3, userPwd);
 
 			rset = pstmt.executeQuery();
