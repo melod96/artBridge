fe330af9a6f637c03d8a96294d75aacbc3f28de8
HyeongWoo <HyeongWoo@DESKTOP-OE0FPJP>
2018-09-22 16:07:40 +0900

    허으어ㅓ~~~~

--- src/com/comvision/artBridge/admin/controller/SelectSearchNoticeServlet.java
+++ src/com/comvision/artBridge/admin/controller/SelectSearchNoticeServlet.java
@@ -22,6 +22,7 @@ public class SelectSearchNoticeServlet extends HttpServlet {
         super();
     }
 
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//페이징 처리
 		int currentPage;
